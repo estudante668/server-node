da6ec54b92eddde1542542dcd15918c59dc94cc1
fabricionetto <agostinhonetto73@gmail.com>
2024-10-03 15:43:16 -0300

    Setimo Commit-recuperar Dados da url

--- servidor.js
+++ servidor.js
@@ -2,6 +2,7 @@
  * 
  */
 const http = require('http');
+const url = require('url');
 
 
 const host = 'localhost';
@@ -9,11 +10,14 @@ const port = 8000;
 
 const dadosRequeridos = function (requisicao, resposta) {
 	resposta.writeHead(200, {'Content-Type': 'text/html'});
-	resposta.write("Url capturada: "+ requisicao.url);
+	let d = url.parse(requisicao.url, true).query;
+	let txt= d.mes + " " + d.ano;
+	resposta.write(txt);
 	resposta.end();
 };
 
 const servidor = http.createServer(dadosRequeridos);
-servidor.listen(port, host, () => {
+
+ servidor.listen(port, host, () => {
     console.log(`Servidor rodando no: http://${host}:${port}`);
 });
